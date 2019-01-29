237f0ca5ca3da51257a421023392235be69e0900
Grzegorz Grzybek <gr.grzybek@gmail.com>
2019-01-29 12:50:08 +0100

    [TOOLS-2] osgi-report-maven-plugin with text report

---
+++ maven/osgi-report-maven-plugin/src/main/java/org/ops4j/tools/maven/osgireport/ReactorSorter.java
@@ -0,0 +1,101 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.ops4j.tools.maven.osgireport;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.maven.AbstractMavenLifecycleParticipant;
+import org.apache.maven.MavenExecutionException;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.Plugin;
+import org.apache.maven.model.PluginExecution;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.component.annotations.Component;
+import org.codehaus.plexus.component.annotations.Requirement;
+import org.slf4j.Logger;
+
+@Component(role = AbstractMavenLifecycleParticipant.class)
+public class ReactorSorter extends AbstractMavenLifecycleParticipant {
+
+    @Requirement
+    private Logger logger;
+
+    @Override
+    public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
+        // we have to:
+        // 1. find a project that declared org.ops4j.tool.maven:osgi-report-maven-plugin plugin
+        //    and manifest-summary goal execution
+        // 2. add dependencies to all other projects within reactor except #1 and reactor's parent.
+
+        MavenProject reportProject = null;
+
+        for (MavenProject project : session.getProjects()) {
+            Plugin plugin = project.getPlugin("org.ops4j.tools.maven:osgi-report-maven-plugin");
+            if (plugin != null) {
+                for (PluginExecution execution : plugin.getExecutions()) {
+                    if (execution.getGoals().stream().anyMatch("manifest-summary"::equals)) {
+                        reportProject = project;
+                        break;
+                    }
+                }
+                if (reportProject != null) {
+                    break;
+                }
+            }
+        }
+
+        if (reportProject == null) {
+            logger.warn("Can't find project with org.ops4j.tools.maven:osgi-report-maven-plugin configured");
+            return;
+        }
+
+        Set<MavenProject> skipped = new HashSet<>();
+        skipped.add(reportProject);
+        MavenProject parent = reportProject.getParent();
+        while (parent != null) {
+            skipped.add(parent);
+            parent = parent.getParent();
+        }
+
+        logger.info("Adding dependencies to " + reportProject);
+
+        for (MavenProject project : session.getProjects()) {
+            if (!skipped.contains(project)) {
+                Dependency dependency = new Dependency();
+                dependency.setGroupId(project.getGroupId());
+                dependency.setArtifactId(project.getArtifactId());
+                dependency.setVersion(project.getVersion());
+                switch (project.getPackaging()) {
+                    case "pom":
+                        dependency.setType("pom");
+                        break;
+                    case "bundle":
+                        dependency.setType("jar");
+                        break;
+                    default:
+                        dependency.setType(project.getPackaging());
+                }
+                reportProject.getDependencies().add(dependency);
+            }
+        }
+    }
+
+}
--- maven/osgi-report-maven-plugin/src/main/java/org/ops4j/tools/maven/osgireport/Summary.java
+++ maven/osgi-report-maven-plugin/src/main/java/org/ops4j/tools/maven/osgireport/Summary.java
@@ -20,15 +20,24 @@ package org.ops4j.tools.maven.osgireport;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.TreeSet;
+import java.util.jar.Attributes;
 import java.util.jar.JarInputStream;
 import java.util.jar.Manifest;
 
+import org.apache.felix.utils.manifest.Attribute;
+import org.apache.felix.utils.manifest.Clause;
+import org.apache.felix.utils.manifest.Directive;
+import org.apache.felix.utils.manifest.Parser;
 import org.apache.maven.artifact.Artifact;
 import org.apache.maven.execution.MavenSession;
 import org.apache.maven.plugin.AbstractMojo;
@@ -39,9 +48,13 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.MavenProjectHelper;
 import org.slf4j.Logger;
 
-@Mojo(name = "manifest-summary", defaultPhase = LifecyclePhase.VERIFY, threadSafe = false)
+/**
+ * Pretty formatting similar to {@code org.apache.karaf.bundle.command.Headers#generateFormattedOutput()}.
+ */
+@Mojo(name = "manifest-summary", defaultPhase = LifecyclePhase.VERIFY, threadSafe = true, inheritByDefault = false, aggregator = true)
 public class Summary extends AbstractMojo {
 
     @Parameter(defaultValue = "${project}", readonly = true)
@@ -50,19 +63,153 @@ public class Summary extends AbstractMojo {
     @Parameter(defaultValue = "${session}", readonly = true)
     private MavenSession session;
 
+    @Parameter(defaultValue = "${project.build.directory}/manifest-summary.txt")
+    private File report;
+
+    @Parameter(defaultValue = "true")
+    private boolean attach;
+
     @Component
     private Logger logger;
 
+    @Component
+    private MavenProjectHelper projectHelper;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
+
         Set<Artifact> allArtifacts = new TreeSet<>(new ArtifactComparator());
         collect(allArtifacts, project.getArtifact(), project.getAttachedArtifacts());
         for (MavenProject p : session.getProjects()) {
             collect(allArtifacts, p.getArtifact(), p.getAttachedArtifacts());
         }
 
-        for (Artifact artifact : allArtifacts) {
-            logger.info("Processing {}", artifact);
+        report.getParentFile().mkdirs();
+
+        try (FileWriter fw = new FileWriter(report)) {
+            for (Artifact artifact : allArtifacts) {
+                logger.info("Processing {}", artifact);
+                fw.write("= " + artifact.toString() + "\n");
+                process(artifact, fw);
+                fw.write("\n\n");
+            }
+        } catch (IOException e) {
+            throw new MojoExecutionException(e.getMessage(), e);
+        }
+
+        if (attach) {
+            logger.info("Attaching " + report);
+            projectHelper.attachArtifact(session.getCurrentProject(), "txt", "manifest-summary", report);
+        }
+    }
+
+    /**
+     * Process {@code META-INF/MANIFEST.MF} from single artifact
+     * @param artifact
+     * @param fw
+     */
+    private void process(Artifact artifact, FileWriter fw) throws IOException {
+        // Bundle-*
+        Map<String, Object> bundleAttributes = new TreeMap<>();
+        // *-Package and Require-Bundle
+        Map<String, Object> packageAttributes = new TreeMap<>();
+        // *-Service
+        Map<String, Object> serviceAttributes = new TreeMap<>();
+        // *-Capability
+        Map<String, Object> capAttributes = new TreeMap<>();
+        // remaining
+        Map<String, Object> otherAttributes = new TreeMap<>();
+
+        File a = artifact.getFile();
+        try (JarInputStream jis = new JarInputStream(new FileInputStream(a))) {
+            Manifest manifest = jis.getManifest();
+            Attributes attrs = manifest.getMainAttributes();
+            for (Object header : attrs.keySet()) {
+                String h = header.toString();
+                if (h.startsWith("Bundle-")) {
+                    bundleAttributes.put(h, attrs.getValue(h));
+                } else if (h.endsWith("-Package") || h.equals("Require-Bundle")) {
+                    packageAttributes.put(h, attrs.getValue(h));
+                } else if (h.endsWith("-Service")) {
+                    serviceAttributes.put(h, attrs.getValue(h));
+                } else if (h.endsWith("-Capability")) {
+                    capAttributes.put(h, attrs.getValue(h));
+                } else {
+                    otherAttributes.put(h, attrs.getValue(h));
+                }
+            }
+        } catch (IOException e) {
+            logger.warn("Can't process {}: {}", artifact.toString(), e.getMessage());
+        }
+
+        fw.write("\n== General attributes\n\n");
+        for (String k : otherAttributes.keySet()) {
+            String v = (String) otherAttributes.get(k);
+            if (v != null) {
+                fw.write(String.format("%s: %s\n", k, v));
+            }
+        }
+
+        fw.write("\n== Bundle attributes\n\n");
+        for (String k : bundleAttributes.keySet()) {
+            String v = (String) bundleAttributes.get(k);
+            if (v != null) {
+                fw.write(String.format("%s: %s\n", k, v));
+            }
+        }
+
+        fw.write("\n== Service attributes\n\n");
+        for (String k : serviceAttributes.keySet()) {
+            String v = (String) serviceAttributes.get(k);
+            if (v != null) {
+                fw.write("\n" + k + ":\n");
+                printFormatted(fw, v);
+            }
+        }
+
+        fw.write("\n== Capabilities attributes\n\n");
+        for (String k : capAttributes.keySet()) {
+            String v = (String) capAttributes.get(k);
+            if (v != null) {
+                fw.write("\n" + k + ":\n");
+                printFormatted(fw, v);
+            }
+        }
+
+        fw.write("\n== Package attributes\n");
+        for (String k : packageAttributes.keySet()) {
+            String v = (String) packageAttributes.get(k);
+            if (v != null) {
+                fw.write("\n" + k + ":\n");
+                printFormatted(fw, v);
+            }
+        }
+    }
+
+    private void printFormatted(FileWriter fw, String value) throws IOException {
+        Clause[] clauses = Parser.parseHeader(value);
+        Arrays.sort(clauses, Comparator.comparing(Clause::getName));
+        for (Clause c : clauses) {
+            fw.write(String.format("    %s\n", c.getName()));
+            Attribute[] atts = c.getAttributes();
+            Directive[] dirs = c.getDirectives();
+            Arrays.sort(atts, Comparator.comparing(Attribute::getName));
+            Arrays.sort(dirs, Comparator.comparing(Directive::getName));
+            for (Attribute at : atts) {
+                fw.write(String.format("        %s = %s\n", at.getName(), at.getValue()));
+            }
+            for (Directive d : dirs) {
+                if ("uses".equals(d.getName())) {
+                    String[] pkgs = d.getValue().split("\\s*,\\s*");
+                    Arrays.sort(pkgs);
+                    fw.write("        uses :=\n");
+                    for (String pkg : pkgs) {
+                        fw.write("            " + pkg + "\n");
+                    }
+                } else {
+                    fw.write(String.format("        %s := %s\n", d.getName(), d.getValue()));
+                }
+            }
         }
     }
 
